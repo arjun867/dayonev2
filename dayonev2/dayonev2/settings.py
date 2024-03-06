4c6b81d685913f8ba813f78b02fd2f08396bcddb
arjun867 <sammengiarjun@gmail.com>
2024-03-06 17:32:42 +0530

    try 0 for aws rds database

--- dayonev2/dayonev2/settings.py
+++ dayonev2/dayonev2/settings.py
@@ -87,25 +87,24 @@ WSGI_APPLICATION = 'dayonev2.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
-
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'database2',
-#         'USER': 'mysuperuser',
-#         'PASSWORD': 'mysuperuser',
-#         'HOST': 'database2.cfm8qyu62dg5.eu-north-1.rds.amazonaws.com',
-#         'PORT': '5432',
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
 # }
 
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'dayonedb',
+        'USER': 'sammengiarjun',
+        'PASSWORD': 'Sa300905',
+        'HOST': 'dayonedb.c386wwm2ubv2.us-east-1.rds.amazonaws.com',
+        'PORT': '5432',
+    }
+}
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
 
