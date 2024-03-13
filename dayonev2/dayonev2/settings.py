601eb0e7ec58b0916b3188c8e04df83a17c4e048
arjun867 <sammengiarjun@gmail.com>
2024-03-13 18:38:59 +0530

    previous color

--- dayonev2/dayonev2/settings.py
+++ dayonev2/dayonev2/settings.py
@@ -88,24 +88,24 @@ WSGI_APPLICATION = 'dayonev2.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'dayonedb',
-        'USER': 'sammengiarjun',
-        'PASSWORD': 'Sa300905',
-        'HOST': 'dayonedb.c386wwm2ubv2.us-east-1.rds.amazonaws.com',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'dayonedb',
+#         'USER': 'sammengiarjun',
+#         'PASSWORD': 'Sa300905',
+#         'HOST': 'dayonedb.c386wwm2ubv2.us-east-1.rds.amazonaws.com',
+#         'PORT': '5432',
+#     }
+# }
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
 
